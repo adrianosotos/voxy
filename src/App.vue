f355b91d338957c9eae06f229776e3a3db632ff2
C1325330 Adriano Matuck Sotos <C1325330@interno.bb.com.br>
2023-02-10 07:57:26 -0300

    feat: removed default component and imported users page

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <UsersPage />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import UsersPage from './pages/UsersPage.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    UsersPage
   },
 })
 export default class App extends Vue {}
