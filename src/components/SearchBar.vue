cb142bcb88a1806999d4a1b7f9f4a46e56371ed0
C1325330 Adriano Matuck Sotos <C1325330@interno.bb.com.br>
2023-02-10 07:58:53 -0300

    feat: search bar component

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <input 
+    type="text"
+    v-model="input"
+    placeholder="Search" 
+    @input="onInputChange"
+/>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { debounce } from '../utils/utils'
+
+@Component
+export default class SearchBar extends Vue {
+  input = '';
+
+  created() {
+    this.onInputChange = debounce(this.onInputChange, 200)
+  }
+
+  onInputChange() {
+   this.$emit('onInputChange', this.input)
+ } 
+}
+</script>
+
+<style>
+
+</style>
