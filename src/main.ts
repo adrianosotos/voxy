124be42f5f2fd5d2fddaa7cca923e925d1d00e5a
C1325330 Adriano Matuck Sotos <C1325330@interno.bb.com.br>
2023-02-10 18:03:40 -0300

    feat: imported faMagnifyingGlass icon

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faArrowDownWideShort, faArrowUpWideShort } from '@fortawesome/free-solid-svg-icons'
+import { faArrowDownWideShort, faArrowUpWideShort, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 Vue.config.productionTip = false
 
 library.add(
   faArrowDownWideShort,
-  faArrowUpWideShort
+  faArrowUpWideShort,
+  faMagnifyingGlass
 )
 
 new Vue({
