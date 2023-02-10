2c0cd692bf861b3fef0a4ecbfece0c3363458c9b
C1325330 Adriano Matuck Sotos <C1325330@interno.bb.com.br>
2023-02-10 07:56:26 -0300

    feat: add font-awesome libraries

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faArrowDownWideShort, faArrowUpWideShort } from '@fortawesome/free-solid-svg-icons'
 
 Vue.config.productionTip = false
+
+library.add(
+  faArrowDownWideShort,
+  faArrowUpWideShort
+)
 
 new Vue({
   render: h => h(App),
