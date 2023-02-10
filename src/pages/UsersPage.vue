a1fd422e5eed5c7fceab66be8d21bb35a64805e0
C1325330 Adriano Matuck Sotos <C1325330@interno.bb.com.br>
2023-02-10 07:55:55 -0300

    feat: users page component

---
+++ src/pages/UsersPage.vue
@@ -0,0 +1,73 @@
+<template>
+  <div v-if="dataSource">
+  <Table 
+    :dataSource="dataSource"
+  ></Table>
+  </div>
+</template>
+
+<script lang="ts">
+interface IUser {
+  email: string;
+  name: string;
+  lastName: string;
+  primaryGroup: string[];
+  phoneNumber: string;
+  hoursStudied: number;
+}
+
+import { Component, Vue } from 'vue-property-decorator'
+import Axios from 'axios';
+
+import Table, { ITableDataSource } from '../components/Table.vue'
+import { environment } from '../environment'
+
+@Component({
+  components: {
+    Table
+  }
+})
+export default class UsersPage extends Vue {
+  private dataSource: ITableDataSource<IUser> | null = null;
+  private columnsConfig = [
+      {
+        key: 'email',
+        title: 'Email'
+      },
+      {
+        key: 'name',
+        title: 'Name'
+      },
+      {
+        key: 'lastName',
+        title: 'Last Name'
+      },
+      {
+        key: 'primaryGroup',
+        title: 'Primary Group'
+      },
+      {
+        key: 'phoneNumber',
+        title: 'Phone'
+      },
+      {
+        key: 'hoursStudied',
+        title: 'Hours Studied',
+        sort: true
+      }
+    ];
+
+  mounted(): void {
+    Axios.get(`${environment.server}/users`).then(({ data }) => {
+      this.dataSource = {
+        columns: this.columnsConfig,
+        data
+      }
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
