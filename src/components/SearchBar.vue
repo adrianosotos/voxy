ae4e1281ca35bbd891b00e3c1851f3cc0f524ebd
C1325330 Adriano Matuck Sotos <C1325330@interno.bb.com.br>
2023-02-10 18:04:11 -0300

    feat: added search icon and styles to SearchBar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,17 +1,28 @@
 <template>
-  <input 
-    type="text"
-    v-model="input"
-    placeholder="Search" 
-    @input="onInputChange"
-/>
+  <div class="search-wrapper">
+    <span class="search-wrapper__search-icon">
+      <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
+    </span>
+    <input 
+      type="text"
+      v-model="input"
+      placeholder="Search" 
+      @input="onInputChange"
+    />
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { debounce } from '../utils/utils'
 
-@Component
+@Component({
+   components: {
+    FontAwesomeIcon
+  }
+}
+)
 export default class SearchBar extends Vue {
   input = '';
 
@@ -25,6 +36,25 @@ export default class SearchBar extends Vue {
 }
 </script>
 
-<style>
+<style lang="scss">
 
+.search-wrapper {
+   position: relative;
+
+  .search-wrapper__search-icon {
+    position: absolute;
+    left: 15px;
+    top: 20px;
+    color: gray;
+  }
+  
+  input {
+    width: 100%; 
+    padding: 12px 40px; 
+    margin: 8px 0; 
+    display: inline-block; 
+    border: 1px solid #ccc; 
+    box-sizing: border-box;
+  }
+}
 </style>
